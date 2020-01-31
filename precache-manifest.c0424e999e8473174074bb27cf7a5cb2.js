203d06301dd595caa2569948cfc523bf3b563358
Peter <peter@Pozsonyi-MacBook-Air.local>
2020-01-31 10:39:50 +0100

    Updates

---
+++ precache-manifest.c0424e999e8473174074bb27cf7a5cb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b268d793792db8e82e65d9e7bed585a3",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "a33e804deccd221a9c4f",
+    "url": "/react-game-of-life/static/js/2.65c4fb16.chunk.js"
+  },
+  {
+    "revision": "3dfc0bb1e86f3fff8848",
+    "url": "/react-game-of-life/static/js/main.e24b9b84.chunk.js"
+  },
+  {
+    "revision": "c746d65d6fe29f5fe771",
+    "url": "/react-game-of-life/static/js/runtime-main.3f58db6f.js"
+  }
+]);
